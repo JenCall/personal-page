dd2468d9247964ee2ed77767bc0647a9eef2b44b
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-10 21:57:13 +0200

    adjust image style

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -5,11 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.scss']
 })
-export class GalleryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class GalleryComponent {
 }
