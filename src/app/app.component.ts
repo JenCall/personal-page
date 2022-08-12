daf02ceaf35722e6758f583955dd8ab4c5462b92
Jeniffer Callou <jenifferprc@gmail.com>
2022-08-12 21:53:13 +0200

    add images info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-page';
+
+  images = [
+    {
+      imageSrc:
+      '../assets/carrousel-img-1.png',
+      imageAlt: 'first-image'
+    },
+    {
+      imageSrc:
+      '../assets/carrousel-img-2.png',
+      imageAlt: 'second-image'
+    },
+    {
+      imageSrc:
+      '../assets/carrousel-img-3.png',
+      imageAlt: 'third-image'
+    }
+  ]
 }
